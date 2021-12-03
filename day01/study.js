3823e033d83afcf624e13d09c48fe4a407bf30f9
deneb1593 <deneb1593@gmail.com>
2021-12-03 10:28:40 +0900

    1일차 커밋

---
+++ day01/study.js
@@ -0,0 +1,13 @@
+console.log('This is my code');
+// 위 구문은 컴퓨터 내부에서 어떠한 기계어로 번역되어 실행될 것이다.
+
+const number = 'Hello';
+console.log(number * number);   //Nah
+console.log(number + 1);        //Hello1
+
+
+const printName = (name) => {
+    console.log(`My name is ${name}`);
+}
+
+printName('Lee');   //Lee
